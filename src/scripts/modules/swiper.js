967bcbb0d665535e40c4f80d5de5b2899b4b0d76
Yuriy Bevov <jury19y@mail.ru>
2022-12-13 19:43:42 +0300

    catalog

---
+++ src/scripts/modules/swiper.js
@@ -0,0 +1,22 @@
+import Swiper, { Navigation } from 'swiper';
+
+const mainSliders = document.querySelectorAll('.main-slider');
+
+if(mainSliders) {
+    mainSliders.forEach(slider => {
+        const buttonNext = slider.parentNode.querySelector('.main-slider-button-next');
+        const buttonPrev = slider.parentNode.querySelector('.main-slider-button-prev');
+
+        new Swiper(slider, {
+            modules: [Navigation],
+
+            slidesPerView: 'auto',
+            spaceBetween: 30,
+
+            navigation: {
+                nextEl: buttonNext,
+                prevEl: buttonPrev,
+            },
+        });
+   });
+}
